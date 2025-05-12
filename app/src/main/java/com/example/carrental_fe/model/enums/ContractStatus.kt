9f631ca1452d680a9a9a7df8555b61a19c8b175f
Nhi <23521113@gm.uit.edu.vn>
2025-05-12 17:32:54 +0700

    Update ContractStatus enum values

--- app/src/main/java/com/example/carrental_fe/model/enums/ContractStatus.kt
+++ app/src/main/java/com/example/carrental_fe/model/enums/ContractStatus.kt
@@ -4,5 +4,5 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 enum class ContractStatus {
-    BOOKED, EXPIRED, COMPLETE, OVERDUE, REVIEWED
+    BOOKED, PICKED_UP, EXPIRED, COMPLETE, OVERDUE, REVIEWED
 }
