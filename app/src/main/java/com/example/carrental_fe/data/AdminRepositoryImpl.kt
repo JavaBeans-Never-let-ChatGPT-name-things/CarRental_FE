2c2cfc98046f738b72723b657cc197a77f4a2420
Umashu-QA <hyugahinataneee@gmail.com>
2025-05-23 22:21:36 +0700

    add Repository for Admin

--- app/src/main/java/com/example/carrental_fe/data/AdminRepositoryImpl.kt
+++ app/src/main/java/com/example/carrental_fe/data/AdminRepositoryImpl.kt
@@ -5,6 +5,11 @@ import com.example.carrental_fe.dto.response.UserDTO
 import com.example.carrental_fe.dto.response.UserDetailDTO
 import com.example.carrental_fe.model.Contract
 import com.example.carrental_fe.network.AdminApi
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.File
 
 class AdminRepositoryImpl(private val adminApi: AdminApi) : AdminRepository {
     override suspend fun getUserList(
@@ -33,4 +38,59 @@ class AdminRepositoryImpl(private val adminApi: AdminApi) : AdminRepository {
             = adminApi.assignContract(contractId, employeeName)
     override suspend fun getAvailableEmployees(contractId: Long): List<String>
             = adminApi.getAvailableEmployees(contractId)
+
+
+    override suspend fun addCarBrand(
+        name: String,
+        logo: File?
+    ): MessageResponse {
+        val namePart = name.toRequestBody("text/plain".toMediaType())
+        val logoPart: MultipartBody.Part? = logo?.let {
+            val requestFile = it.asRequestBody("image/*".toMediaType())
+            MultipartBody.Part.createFormData("logo", it.name, requestFile)
+        }
+        return adminApi.addCarBrand(
+            name = namePart,
+            logo = logoPart
+        )
+    }
+
+    override suspend fun addCar(
+        id: String,
+        brandName: String,
+        maxSpeed: Float,
+        carRange: Float,
+        carImage: File?,
+        seatsNumber: Int,
+        rentalPrice: Float,
+        engineType: String,
+        gearType: String,
+        drive: String
+    ): MessageResponse {
+        val idPart = id.toRequestBody("text/plain".toMediaType())
+        val brandPart = brandName.toRequestBody("text/plain".toMediaType())
+        val maxSpeedPart = maxSpeed.toString().toRequestBody("text/plain".toMediaType())
+        val carRangePart = carRange.toString().toRequestBody("text/plain".toMediaType())
+        val seatsNumberPart = seatsNumber.toString().toRequestBody("text/plain".toMediaType())
+        val rentalPricePart = rentalPrice.toString().toRequestBody("text/plain".toMediaType())
+        val engineTypePart = engineType.toRequestBody("text/plain".toMediaType())
+        val gearTypePart = gearType.toRequestBody("text/plain".toMediaType())
+        val drivePart = drive.toRequestBody("text/plain".toMediaType())
+        val carImagePart: MultipartBody.Part? = carImage?.let {
+            val requestFile = it.asRequestBody("image/*".toMediaType())
+            MultipartBody.Part.createFormData("carImage", it.name, requestFile)
+        }
+        return adminApi.addCar(
+            id = idPart,
+            brandName = brandPart,
+            maxSpeed = maxSpeedPart,
+            carRange = carRangePart,
+            carImage = carImagePart,
+            seatsNumber = seatsNumberPart,
+            rentalPrice = rentalPricePart,
+            engineType = engineTypePart,
+            gearType = gearTypePart,
+            drive = drivePart
+        )
+    }
 }
