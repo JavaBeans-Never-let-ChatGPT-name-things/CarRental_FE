c6772de1f4a23e7a5391129dae5eee5de913c8c4
ShiniThinh <slimerimuru2005@gmail.com>
2025-05-25 10:57:36 +0700

    Modify Admin Repository

--- app/src/main/java/com/example/carrental_fe/data/AdminRepository.kt
+++ app/src/main/java/com/example/carrental_fe/data/AdminRepository.kt
@@ -1,5 +1,11 @@
 package com.example.carrental_fe.data
 
+import com.example.carrental_fe.dto.request.ReportRequestDTO
+import com.example.carrental_fe.dto.response.CarSummaryDTO
+import com.example.carrental_fe.dto.response.ContractSummaryDTO
+import com.example.carrental_fe.dto.response.MonthlyReportDTO
+import com.example.carrental_fe.dto.response.ReturnStatusDTO
+import com.example.carrental_fe.dto.response.UserSummaryDTO
 import com.example.carrental_fe.dto.response.MessageResponse
 import com.example.carrental_fe.dto.response.UserDTO
 import com.example.carrental_fe.dto.response.UserDetailDTO
@@ -53,4 +59,28 @@ interface AdminRepository {
         gearType: String,
         drive: String,
     ): MessageResponse
+
+    suspend fun getTop3BestUsers(): List<UserSummaryDTO>
+    suspend fun getTop3WorstUsers(): List<UserSummaryDTO>
+    suspend fun getTop3BestUsersFromDateToDate(reportRequestDTO: ReportRequestDTO) : List<UserSummaryDTO>
+    suspend fun getTop3WorstUsersFromDateToDate(reportRequestDTO: ReportRequestDTO) : List<UserSummaryDTO>
+    suspend fun getTop3RentedCars(): List<CarSummaryDTO>
+    suspend fun getTop3RentedCarsFromDateToDate(reportRequestDTO: ReportRequestDTO) : List<CarSummaryDTO>
+    suspend fun getTop3RatingCars(): List<CarSummaryDTO>
+    suspend fun getTop3RatingCarsFromDateToDate(reportRequestDTO: ReportRequestDTO) : List<CarSummaryDTO>
+
+    suspend fun getMonthlyTotalRevenue(year: Int): List<MonthlyReportDTO>
+    suspend fun getMonthlyTotalPenalty(year: Int): List<MonthlyReportDTO>
+
+    suspend fun getTotalRevenueFromDateToDate(reportRequestDTO: ReportRequestDTO): Double
+    suspend fun getTotalPenaltyFromDateToDate(reportRequestDTO: ReportRequestDTO): Double
+
+    suspend fun getTotalRevenue(): Double
+    suspend fun getTotalPenalty(): Double
+
+    suspend fun getContractSummary(): List<ContractSummaryDTO>
+    suspend fun getContractSummaryFromDateToDate(reportRequestDTO: ReportRequestDTO): List<ContractSummaryDTO>
+
+    suspend fun getReturnStatus(): List<ReturnStatusDTO>
+    suspend fun getReturnStatusFromDateToDate(reportRequestDTO: ReportRequestDTO): List<ReturnStatusDTO>
 }
