2b1bb95b350e29731808ab1411eca394c442f67e
Nhi <23521113@gm.uit.edu.vn>
2025-05-27 09:07:12 +0700

    Add change password logic in Profile screen

--- app/src/main/java/com/example/carrental_fe/screen/user/userProfile/ProfileScreenViewModel.kt
+++ app/src/main/java/com/example/carrental_fe/screen/user/userProfile/ProfileScreenViewModel.kt
@@ -11,16 +11,27 @@ import com.example.carrental_fe.CarRentalApplication
 import com.example.carrental_fe.data.AccountRepository
 import com.example.carrental_fe.data.AuthenticationRepository
 import com.example.carrental_fe.data.TokenManager
+import com.example.carrental_fe.dto.request.EmailRequest
+import com.example.carrental_fe.dto.response.MessageResponse
 import com.example.carrental_fe.model.Account
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
+sealed class ForgotPasswordProfileState {
+    object Idle : ForgotPasswordProfileState()
+    object Loading : ForgotPasswordProfileState()
+    data class Success(val messageResponse: MessageResponse) : ForgotPasswordProfileState()
+    data class Error(val message: String) : ForgotPasswordProfileState()
+}
 class ProfileScreenViewModel(private val accountRepository: AccountRepository,
                              private val tokenManager: TokenManager,
                              private val authenticationRepository: AuthenticationRepository): ViewModel() {
     private val _accountInfo = MutableStateFlow<Account?>(null)
     val accountInfo: StateFlow<Account?> = _accountInfo
+    private val _forgotPasswordState = MutableStateFlow<ForgotPasswordProfileState>(ForgotPasswordProfileState.Idle)
+    val forgotPasswordState: StateFlow<ForgotPasswordProfileState> = _forgotPasswordState
+
     init {
         getAccountInfo()
     }
@@ -45,6 +56,25 @@ class ProfileScreenViewModel(private val accountRepository: AccountRepository,
                 tokenManager.clearTokens()
             }
             onFinished()
+        }
+    }
+    fun sendEmail()
+    {
+        viewModelScope.launch {
+            _forgotPasswordState.value = ForgotPasswordProfileState.Loading
+            try {
+                val response = authenticationRepository.forgot(
+                    EmailRequest(_accountInfo.value?.email?:"")
+                )
+                if(response.message == "Forgot password email sent")
+                {
+                    _forgotPasswordState.value = ForgotPasswordProfileState.Success(response)
+                }
+            }
+            catch (e: Exception)
+            {
+                _forgotPasswordState.value = ForgotPasswordProfileState.Error(e.message?: "An error occurred")
+            }
         }
     }
     companion object {
